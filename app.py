80909902c0892e13687b0f4b24d82c3b9b962fa5
koansito <chronwasclosed@gmail.com>
2026-03-19 07:45:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 #from PIL import Image
 from PIL import Image as Image, ImageOps as ImagOps
 from keras.models import load_model
+from PIL import Image
 
 import platform
 
@@ -13,10 +14,10 @@ st.write("Versión de Python:", platform.python_version())
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-st.title("Reconocimiento de Imágenes")
+st.title("q(≧▽≦q) reconociendo imagenes yayyyyy")
 #st.write("Versión de Python:", platform.python_version())
-image = Image.open('OIG5.jpg')
-st.image(image, width=350)
+image = Image.open('pose.jpg')
+st.image(image, width=600)
 with st.sidebar:
     st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
 img_file_buffer = st.camera_input("Toma una Foto")
